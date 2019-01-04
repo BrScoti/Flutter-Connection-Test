fe2d06d9a69753bac34f0f04a69e531c32ee2981
Bruno Scoti <bruno.scoti@yahoo.com.br>
2019-01-04 16:53:54 -0200

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,44 +10,32 @@ void main() {
   connectionStatus.initialize();
 
   runApp(MyApp());
-
-  //connectionStatus.dispose();
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+       primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(),
         body: Container(
-          child: TestWidget(),
+          child: IsConnect(),
         ),
       ),
     );
   }
 }
 
-class TestWidget extends StatefulWidget {
+class IsConnect extends StatefulWidget {
   @override
-  TestWidgetState createState() => new TestWidgetState();
+  IsConnectState createState() => new IsConnectState();
 }
 
-class TestWidgetState extends State<TestWidget> {
+class IsConnectState extends State<IsConnect> {
   StreamSubscription _connectionChangeStream;
 
   bool isOffline = false;
